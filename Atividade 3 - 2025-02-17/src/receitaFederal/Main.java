1f0bf6c4ffc2ada2bde2a1ae4913c1899bec8c69
isabellexvr <isabellexavier59@gmail.com>
2025-02-21 22:20:10 -0300

    fix: atividade 3 prints

--- Atividade 3 - 2025-02-17/src/receitaFederal/Main.java
+++ Atividade 3 - 2025-02-17/src/receitaFederal/Main.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 
 public class Main {
     public static PessoaJuridica createPJ(Scanner sc){
-        System.out.println("Insira o nome do pessoa jurídica:");
+        System.out.println("Name:");
         String nome = sc.nextLine();
-        System.out.println("Insira a renda anual da pessoa:");
+        System.out.println("Anual income:");
         double renda = sc.nextDouble();
-        System.out.println("Insira a quantidade de funcionários da empresa:");
+        System.out.println("Number of employees:");
         int quantidade = sc.nextInt();
         sc.nextLine();
 
@@ -17,11 +17,11 @@ public class Main {
     }
 
     public static PessoaFisica createPF(Scanner sc){
-        System.out.println("Insira o nome do pessoa fisica:");
+        System.out.println("Name:");
         String nome = sc.nextLine();
-        System.out.println("Insira a renda anual da pessoa:");
+        System.out.println("Anual income:");
         double renda = sc.nextDouble();
-        System.out.println("Insira os gastos com saúde da pessoa:");
+        System.out.println("Health expenditures:");
         double gastosComSaude = sc.nextDouble();
         sc.nextLine();
 
@@ -30,7 +30,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Quantos contribuintes serão inseridos? (Valor em número)");
+        System.out.println("Enter the number of tax payers: ");
         Scanner sc = new Scanner(System.in);
         int numContribuintes = sc.nextInt();
         sc.nextLine();
@@ -38,28 +38,39 @@ public class Main {
         ArrayList<Pessoa> contribuintes = new ArrayList<Pessoa>();
 
         for (int i = 0; i < numContribuintes; i++) {
-            System.out.println("Cadastro da pessoa " + i+1 + " :\n");
-            System.out.println("Trata-se de uma pessoa física ou de uma pessoa jurídica? (Responda com PJ ou PF)");
-            String tipoPessoa = sc.nextLine();
-            if(tipoPessoa.equalsIgnoreCase("pj")){
-                PessoaJuridica novaPJ = createPJ(sc);
-                contribuintes.add(novaPJ);
-            }else if(tipoPessoa.equalsIgnoreCase("pf")){
-                PessoaFisica novaPF =  createPF(sc);
-                contribuintes.add(novaPF);
+            System.out.println("Tax payer #" + (i + 1) + " data:");
+
+            String tipoPessoa;
+            while (true) {
+                System.out.println("Individual or company (i/c)?");
+                tipoPessoa = sc.nextLine();
+
+                if (tipoPessoa.equalsIgnoreCase("c")) {
+                    PessoaJuridica novaPJ = createPJ(sc);
+                    contribuintes.add(novaPJ);
+                    break;
+                } else if (tipoPessoa.equalsIgnoreCase("i")) {
+                    PessoaFisica novaPF = createPF(sc);
+                    contribuintes.add(novaPF);
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter 'i' for individual or 'c' for company.\n");
+                }
             }
         }
 
+
         double valorToral = 0;
 
+        System.out.println("TAXES PAID:");
         for(Pessoa pessoa : contribuintes){
-            System.out.printf("Valor de impostos pago por %s: R$ %.2f\n", pessoa.getNome(), pessoa.calcularImposto());
+            System.out.printf("%s: $ %.2f\n", pessoa.getNome(), pessoa.calcularImposto());
             //System.out.println("Valor de impostos pago por " + pessoa.getNome() + ": R$" + pessoa.calcularImposto());
             valorToral += pessoa.calcularImposto();
         }
 
         //System.out.println("Valor total arrecadado de impostos: R$" + valorToral);
-        System.out.printf("Valor total arrecadado de impostos: R$%.2f\n", valorToral);
+        System.out.printf("TOTAL TAXES: $ %.2f\n", valorToral);
 
     }
 }
